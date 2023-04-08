736ab19397b4534b19a54ed564a3dae02ba6b224
fehertoypeter <fehertoypeter@gmail.com>
2023-04-08 15:49:18 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -534,6 +534,31 @@ countdownBox.style.display = "block";
 
 
 
+let proba = [
+	{
+		subject: "REPÜLÉSI SZABÁLYOK",
+		question: "Milyen minimális megengedett látástávolságig lehet üzemelni F-osztályú légtérben a látvarepülési szabályok szerint?",
+		answers: [
+			{ text: "5 km.", correct: false},
+			{ text: "5 km, de ha a repülést olyan sebességgel hajtják végre, ami lehetővé teszi az egyéb forgalom vagy akadályok időbeni észlelését és az összeütközés elkerülését, akkor 1500 m.", correct: false},
+			{ text: " 8 km FL100-on és felette, 5 km FL100 alatt.", correct: true},
+			{ text: "8 km.", correct: false},
+		],
+		hint:"",
+	}
+];
+
+function updateJSON() {
+  var hint = document.getElementById("hintField").value;
+  proba[0].hint = hint;
+  var jsonData = JSON.stringify(questions);
+
+  var element = document.getElementById("output");
+  element.innerHTML = jsonData;
+}
+
+
+
 
 
 
